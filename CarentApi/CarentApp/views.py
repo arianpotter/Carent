45ad05359ebc82a7b833f925df0b2d0f5eb73b58
Arian Hadi <arian.hadi78@gmail.com>
2022-07-02 14:55:18 +0430

    RentView initialized

--- CarentApi/CarentApp/views.py
+++ CarentApi/CarentApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets
-from CarentApp.models import User
+from CarentApp.models import User,RentalCar,Rent
 from rest_framework.decorators import action
-from .serializers import UserSerializer
+from .serializers import UserSerializer,RentSerializer,RentalCarSerializer
 from django.contrib.auth.models import AnonymousUser
 # Create your views here.
 
@@ -23,4 +23,10 @@ class RentalCarView(viewsets.ViewSet):
     pass
 
 class RentView(viewsets.ViewSet):
-    pass
+    def list(self,request):
+        if(request.user != AnonymousUser()):
+            rents = Rent.objects.filter(client__exact=request.user)
+            serializer = RentSerializer(rents,many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status=401)
